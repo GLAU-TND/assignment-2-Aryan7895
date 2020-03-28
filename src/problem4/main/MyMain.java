3bc4cee2e4812214e5adac438bdedbe6d3469e49
Dell <saxena.aryan2000@gmail.com>
2020-03-28 13:31:56 +0530

    my  queue

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -5,7 +5,25 @@
  *  Time: 7:08 PM
  */
 package problem4.main;
+
+import problem1.mybst.MyBinarySearchTree;
+import problem4.myqueue.MyQueue;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree obj = new MyBinarySearchTree();
+        obj.insert(11);
+        obj.insert(4);
+        obj.insert(33);
+        obj.insert(37);
+        obj.insert(14);
+        obj.insert(1);
+        obj.insert(9);
+
+        MyQueue mq = new MyQueue();
+        mq.preSuccessor(obj.getroot());
+        mq.getPreSucc();
+    }
 }
